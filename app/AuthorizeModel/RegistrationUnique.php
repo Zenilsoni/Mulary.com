780a3b7b08e7a0fe3f936aab22acdd69c1a7aabc
Zenil9749 <zenil9749@gmail.com>
2020-02-16 02:30:03 -0500

    Revert "validation 2nd commit"

--- app/AuthorizeModel/RegistrationUnique.php
+++ app/AuthorizeModel/RegistrationUnique.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class RegistrationUnique extends Model
 {
     protected $table="registrations";
-    protected $primaryKey = "signup_id";
-    protected $fillable=["username","firstname","lastname","email","college_id","gender","terms","status"];
+    protected $fillable=["username","email"];
 }
--- app/Http/Controllers/Authorization/RegisterController.php
+++ app/Http/Controllers/Authorization/RegisterController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Authorization;
 
-use App\AuthorizeModel\RegistrationUnique;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\validation\BasicInfoValidateRequest;
 use Illuminate\Http\Request;
@@ -12,18 +11,6 @@ class RegisterController extends Controller
     function insert(BasicInfoValidateRequest $request)
     {
         $validate = $request->validated();
-
-$signup = new RegistrationUnique;
-
-
-        $signup->username=$request->input('u_name');
-        $signup->firstname=$request->input('u_fname');
-        $signup->lastname=$request->input('u_lname');
-        $signup->email=$request->input('u_email');
-        $signup->college_id=$request->input('user_type_college');
-        $signup->gender=$request->input('gender');
-        $signup->terms=$request->input('u_accept');
-
-        $signup->save();
+    echo "Here".$validate;
     }
 }
