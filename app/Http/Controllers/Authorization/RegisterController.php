ead6281eaba3d535740c01194d5eff78ecef4c72
Zenil Soni <zenil9749@gmail.com>
2020-02-19 19:30:14 -0500

    validation of fields

--- app/Http/Controllers/Authorization/RegisterController.php
+++ app/Http/Controllers/Authorization/RegisterController.php
@@ -14,36 +14,44 @@ class RegisterController extends Controller
 function view(){
     return view('userPages.forms.signup');
 }
-//    function insert(BasicInfoValidateRequest $request)
-//    {
-//    //    $validate = $request->validated();
-//        $input= Input::all();
-//        console.log($input);
-//    }
-    function insert(Request $request)
+    function insert(BasicInfoValidateRequest $request)
     {
-//        $email = $request->get('u_email');
-//         $fname = $request->get('u_fname');
-//         $lname= $request->get('u_lname');
-//         $cnt = $request->get('u_phno');
-
-
-
-//$check = $request->get('u_accept');
-
-
-
+        $validate = $request->validated();
         $signup = new registrations();
-        $signup->email=$request->input('u_email');
-        $signup->firstname=$request->input('u_fname');
-        $signup->lastname=$request->input('u_lname');
-        $signup->contact=$request->input('u_phone');
+        $signup->email=$validate->input('u_email');
+        $signup->firstname=$validate->input('u_fname');
+        $signup->lastname=$validate->input('u_lname');
+        $signup->contact=$validate->input('u_phone');
         $signup->school_id=10;
         $signup->occupations='zzzzz';
-        $signup->terms=$request->input('u_accept');
+        $signup->terms=$validate->input('u_accept');
         $signup->status='kaib';
         $signup->save();
-//echo "<h1>$check</h1>";
-
     }
+//    function insert(Request $request)
+//    {
+////        $email = $request->get('u_email');
+////         $fname = $request->get('u_fname');
+////         $lname= $request->get('u_lname');
+////         $cnt = $request->get('u_phno');
+//
+//
+//
+////$check = $request->get('u_accept');
+//
+//
+//
+//        $signup = new registrations();
+//        $signup->email=$request->input('u_email');
+//        $signup->firstname=$request->input('u_fname');
+//        $signup->lastname=$request->input('u_lname');
+//        $signup->contact=$request->input('u_phone');
+//        $signup->school_id=10;
+//        $signup->occupations='zzzzz';
+//        $signup->terms=$request->input('u_accept');
+//        $signup->status='kaib';
+//        $signup->save();
+////echo "<h1>$check</h1>";
+//
+//    }
 }
